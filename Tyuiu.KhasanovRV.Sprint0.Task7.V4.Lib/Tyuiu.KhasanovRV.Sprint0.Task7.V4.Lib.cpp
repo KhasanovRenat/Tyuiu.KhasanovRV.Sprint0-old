03e79c0d60c133b43e3e062c578431f12ee08b84
Ренат Хасанов <Tyuiu.KhasanovRV@outlook.com>
2023-09-14 16:04:18 +0500

    Task7

---
+++ Tyuiu.KhasanovRV.Sprint0.Task7.V4.Lib/Tyuiu.KhasanovRV.Sprint0.Task7.V4.Lib.cpp
@@ -0,0 +1,28 @@
+// Tyuiu.KhasanovRV.Sprint0.Task4.V4.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: This is an example of a library function
+class Service1 :public ISprint0Task7
+{
+	virtual bool Rezalt(int a, int b) override
+	{
+		int aa = a;
+		while (aa > 0)
+		{
+			int num = aa % 10;
+			if (num == b)
+			{
+				return true;
+			}
+			
+			aa /= 10;
+		}
+
+		return false;
+	};
+
+};
---
+++ Tyuiu.KhasanovRV.Sprint0.Task7.V4.Test/Tyuiu.KhasanovRV.Sprint0.Task7.V4.Test.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.KhasanovRV.Sprint0.Task7.V4.Lib/Tyuiu.KhasanovRV.Sprint0.Task7.V4.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest4
+{
+	TEST_CLASS(UnitTest4)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+
+	};
+}
